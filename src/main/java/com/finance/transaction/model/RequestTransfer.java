8fec3d7a9a134630283b76827f38ba2ca626c36c
douglas.santos <douglas.santos@nava.com.br>
2023-03-05 22:16:34 -0300

    Mudança de bibliotecas e ajustes no objeto de request

--- src/main/java/com/finance/transaction/model/RequestTransfer.java
+++ src/main/java/com/finance/transaction/model/RequestTransfer.java
@@ -1,4 +1,23 @@
 package com.finance.transaction.model;
 
+import lombok.Data;
+
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+@Data
 public class RequestTransfer {
+
+    @NotBlank(message = "Origin Account cannot be blank")
+    String originAccount;
+    @NotBlank(message = "Destination Account cannot be blank")
+    String destinationAccount;
+    @NotBlank(message = "value cannot be blank")
+    double value;
+    @NotBlank(message = "Date Transfer cannot be blank")
+    LocalDateTime dateTransfer;
+    @NotBlank(message = "Date Scheduling cannot be blank")
+    LocalDateTime dateScheduling;
+
+
 }
